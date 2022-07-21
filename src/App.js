2f6ff711f30393849ce0294b76340f0715c2356b
tasoko <57208858+tasoko-nao@users.noreply.github.com>
2022-07-21 15:47:15 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-import { useCallback, useMemo, useState } from "react";
-import { ChildArea } from "./ChildArea";
+import { CssModules } from "./components/CssModules";
+import { Emotion } from "./components/Emotion";
+import { InlineStyle } from "./components/InlineStyle";
+import { StyledComponents } from "./components/StyledComponents";
+import { StyledJsx } from "./components/StyledJsx";
 import "./styles.css";
 
 export default function App() {
-  const [text, setText] = useState("");
-  const [open, setOpen] = useState(false);
-
-  const onChangeText = (e) => {
-    setText(e.target.oepn);
-  };
-  const onClickOpen = () => {
-    setOpen(!open);
-  };
-  const onClickClose = useCallback(() => {
-    setOpen(false);
-  }, [setOpen]);
-
-  const temp = useMemo(() => 1 + 3, []);
-  console.log(temp);
-
   return (
     <div className="App">
-      <input value={text} onChange={onChangeText} />
-      <br />
-      <br />
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose} />
+      <InlineStyle />
+      <CssModules />
+      <StyledJsx />
+      <StyledComponents />
+      <Emotion />
     </div>
   );
 }
